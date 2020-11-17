c8f02b1f500ae0b2278d425aa0516439f1f9009f
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2020-11-17 10:02:35 +0100

    Esercizi Tutorato 2

---
+++ Tutorato_2/ListenerEsempio/src/Esempio.java
@@ -0,0 +1,67 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.ActionEvent;
+
+public class Esempio {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Esempio window = new Esempio();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Esempio() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				System.out.println("Focus Gained");
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				System.out.println("Focus Lost");				
+			}
+			
+		});
+		frame.getContentPane().add(btnNewButton, BorderLayout.CENTER);
+	}
+
+}
