86852552907c7ae56a354d7d9bf225b807a75a11
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2020-12-15 22:34:44 +0100

    First App REST

---
+++ Tutorato_6/myFirstApp/src/main/java/com/example/restservice/MyFirstAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MyFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstAppApplication.class, args);
+	}
+
+}
